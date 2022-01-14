9d26b1eea384c5f5bc7fb132652e15d5ee495986
Mikhail <skakunovmy@gmail.com>
2022-01-15 02:12:31 +0300

    lesson04

---
+++ lesson-04-spring-boot/src/main/java/ru/geekbrains/persist/ProductRepositoryImpl.java
@@ -0,0 +1,51 @@
+package ru.geekbrains.persist;
+
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class ProductRepositoryImpl implements ProductRepository{
+
+    private final Map<Long, Product> productMap = new ConcurrentHashMap<>();
+    private final AtomicLong identity = new AtomicLong();
+
+    @PostConstruct
+    public void init() {
+        this.save(new Product(null, "product 1", "description 1", new BigDecimal("1300")));
+        this.save(new Product(null, "product 2", "description 2", new BigDecimal("1250")));
+        this.save(new Product(null, "product 3", "description 3", new BigDecimal("3845.54")));
+        this.save(new Product(null, "product ", "description", new BigDecimal("4300.39")));
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return new ArrayList<>(productMap.values());
+    }
+
+    @Override
+    public Optional<Product> findById(long id) {
+        return Optional.ofNullable(productMap.get(id));
+    }
+
+    @Override
+    public void save(Product product) {
+        if (product.getId() == null) {
+            long id = identity.incrementAndGet();
+            product.setId(id);
+        }
+        productMap.put(product.getId(), product);
+    }
+
+    @Override
+    public void delete(long id) {
+        productMap.remove(id);
+    }
+}
